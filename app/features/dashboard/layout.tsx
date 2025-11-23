5c8c792a07d51f886e96842a322aa1bb013a000d
Calderaly <calderaly16@gmail.com>
2025-11-23 14:50:18 +0700

    feat: create dashboard layout and dashboard style

--- app/features/dashboard/layout.tsx
+++ app/features/dashboard/layout.tsx
@@ -1,3 +1,20 @@
+import type { Route } from '.react-router/types/app/+types/root';
+import '@/css/dashboard.css'
+import { Outlet } from 'react-router';
+
+export const links: Route.LinksFunction = () => [
+	{ rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+	{
+		rel: 'preconnect',
+		href: 'https://fonts.gstatic.com',
+		crossOrigin: 'anonymous',
+	},
+	{
+		rel: 'stylesheet',
+		href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
+	},
+];
+
 export default function DashboardLayout() {
-	return <h1> hello world </h1>;
+	return <Outlet/>
 }
